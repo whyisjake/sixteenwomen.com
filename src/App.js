ecb4d89b424ed346bd17da3f0f476e164b89cad6
Jake Spurlock <whyisjake@gmail.com>
2018-06-26 21:10:57 -0700

    Scaffold in a few things.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Navbar from "./components/navbar";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Sixteen Women</h1>
-        </header>
-        <p className="App-intro">
-          Jake was here
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div>
+      <Navbar />
+
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/episodes" component={Episodes} />
+    </div>
+  </Router>
+);
+
+const Home = () => (
+  <div className="container">
+    <h2>Home</h2>
+  </div>
+);
+
+const About = () => (
+  <div className="container">
+    <h1>About</h1>
+    <p>Narrative and Storytelling Lab project by Caitlin Stetson, Jake Spurlock, Paige Sullivan, and Tom Henkenius.</p>
+    <p>More info <a href="https://design.usc.edu">here</a></p>
+  </div>
+);
+
+const Episodes = ({ match }) => (
+  <div className="container">
+    <h2>Episodes</h2>
+    <ul>
+      <li>
+        Episode 1: Christina Marie Riggs
+      </li>
+      <li>
+        TK
+      </li>
+      <li>
+        TK
+      </li>
+    </ul>
+  </div>
+);
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+);
 
 export default App;
---
+++ src/components/navbar.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+
+export default class Example extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">Sixteen Women</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <Link className="nav-link" to="/">Home</Link>
+              </NavItem>
+              <NavItem>
+                <Link className="nav-link" to="/about">About</Link>
+              </NavItem>
+              <NavItem>
+                <Link className="nav-link" to="/episodes">Episodes</Link>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
