80b8e6fa04dd43da903ad0aa198189b641f49691
Jake Spurlock <whyisjake@gmail.com>
2018-07-16 14:50:26 -0700

    Fix the stuff.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./theme/css/bootstrap.css";
-import "./theme/css/font-awesome.css";
-import "./theme/css/replacer.css";
-import "./theme/css/site-style.css";
+// import "import 'bootstrap/dist/css/bootstrap.css";
+// import "import 'bootstrap/dist/css/font-awesome.css";
+// import "import 'bootstrap/dist/css/replacer.css";
+// import "import 'bootstrap/dist/css/site-style.css";
 
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
